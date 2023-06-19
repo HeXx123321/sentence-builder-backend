800d7289064d583c859ecd5ccea8fd1bde393d56
Hexx123321 <thulane.alfred@gmail.com>
2023-06-19 14:05:37 +0200

    [0.2.1] Partial Sentence Route Update

--- src/routes/partial_sentences.js
+++ src/routes/partial_sentences.js
@@ -1,13 +1,20 @@
 const express = require('express')
 const router = express.Router()
-//const partial_sentences = require('../modelz')
+const Sentence = require('../models/sentences/sentence')
+const partialSentence = require('../models/partialsentence')
 // Simplicity is better here
 
 // Get all
-router.get('/', (req, res) => {
-    res.send('hell')
-
+router.get('/', async (req, res) => {
+    try {
+        const partialSentence = await partialSentence.find()
+        res.json(partialSentence)
+  
+      } catch (err) {
+        res.status(500).json({ message: err.message })
+      }
 })
+
 // Getting one
 router.get('/:id', (req, res) => {
     res.send(req.params.id)
